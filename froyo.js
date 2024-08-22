e8567fbf7e934f61d6d64109211051392c41a960
Andrew Yang <ayang312@gmail.com>
2024-08-22 19:43:38 -0400

    edited

--- froyo.js
+++ froyo.js
@@ -1,31 +1,28 @@
-const flavors = prompt("Please enter a list of comma-separated Froyo flavors (no spaces)");
+let flavors = prompt("Please enter a list of comma-separated Froyo flavors (no spaces)");
 
 // parse input into an array of flavor
-const inputFlavor = flavors.split(",");
+flavors = flavors.split(",");
 
 //create object for flavors
-const froyo = {};
+const flavorCount = (arr) => {
 
-//you are looping through the array
-//loop of array and determine if flavor exists and increase count
-//does the key vanilla exist in my froyo object?.. if not, create key and set value to 1
-//if key vanilla does exist, increase value by 1
-let vanillaCount = 0;
-let strawberryCount = 0;
-let coffeeCount = 0;
+    const count = {};
 
-for (let i = 0; i < inputFlavor.length; i++) {
-    if (inputFlavor[i].toLowerCase() === "vanilla") {
-        vanillaCount += 1;
-        froyo["vanilla"] = vanillaCount;       
-    }   else if (inputFlavor[i].toLowerCase() === "strawberry") {
-        strawberryCount += 1;
-        froyo["strawberry"] = strawberryCount;
-    } else if (inputFlavor[i].toLowerCase() === "coffee") {
-        coffeeCount += 1;
-        froyo["coffee"] = coffeeCount;
+    //you are looping through the array
+    //loop of array and determine if flavor exists and increase count
+    //does the key vanilla exist in my froyo object?.. if not, create key and set value to 1
+    //if key vanilla does exist, increase value by 1
+    for (let flavor of arr) {
+        flavor = flavor.trim();
+            if (flavor in count) {
+                count[flavor] += 1;
+            } else {
+                count[flavor] = 1;
+            }
     }
+    return count;
 }
 
+
 // return object
-console.table(froyo);
+console.table(flavorCount(flavors));
